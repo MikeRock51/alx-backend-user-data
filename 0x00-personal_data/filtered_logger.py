f161da357f222e28b531ac16d602c8023a8e508c
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-03 05:49:19 +0100

    Add Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,10 +17,32 @@
 
 import logging
 import re
+from typing import List
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        msg = filter_datum(self.fields, self.REDACTION, record.msg,
+                           self.SEPARATOR)
+        record.msg = msg
+        message = super().format(record)
+
+        return message
 
 
 def filter_datum(fields: int, redaction: str,
-                 message: str, seperator: str) -> None:
+                 message: str, seperator: str) -> str:
     """Uses regex to replace occuerences of certain values"""
 
     for field in fields:
