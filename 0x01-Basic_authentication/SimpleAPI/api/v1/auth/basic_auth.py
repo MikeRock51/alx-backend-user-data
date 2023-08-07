eac331a570438a5a2cd1a211e26a9977beca3b8a
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-07 16:01:38 +0100

    Add Task 10

--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -3,6 +3,9 @@
 
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import TypeVar
+from models.user import User
+from models.base import Base
 
 
 class BasicAuth(Auth):
@@ -38,3 +41,23 @@ class BasicAuth(Auth):
             return (None, None)
 
         return tuple(decoded_base64_authorization_header.split(":"))
+
+    def user_object_from_credentials(self, user_email: str, user_pwd: str
+                                     ) -> TypeVar('User'):
+        """Returns a User instance based on email and password"""
+        if not user_email or type(user_email) != str:
+            return None
+        if not user_pwd or type(user_pwd) != str:
+            return None
+
+        user: User = User().search({"email": user_email})
+
+        if not user or len(user) == 0:
+            return None
+
+        user = user[0]
+
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
