63f395722abd14a2c2a40856c5fdaed0f7c512d7
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-08 04:16:27 +0100

    Add Task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -13,11 +14,14 @@ class Auth:
         if not path or not excluded_paths or len(excluded_paths) < 1:
             return True
 
-        if path[-1] != '/':
-            path = path + '/'
-
-        if path in excluded_paths:
+        if any(re.match(pattern, path) for pattern in excluded_paths):
             return False
+
+        # if path[-1] != '/':
+        #     path = path + '/'
+
+        # if path in excluded_paths:
+        #     return False
         return True
 
     def authorization_header(self, request=None) -> str:
