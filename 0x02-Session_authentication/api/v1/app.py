73da1b569f9a9a8788fdaca4689a07535459a4f4
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-09 12:51:09 +0100

    Add Task 1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -20,6 +20,9 @@ if auth:
     if auth == "basic_auth":
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
+    elif auth == "session_auth":
+        from api.v1.auth.session_auth import SessionAuth
+        auth = SessionAuth()
     else:
         from api.v1.auth.auth import Auth
         auth = Auth()
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Session Authentication Module"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Handles session authentication"""
+    pass
