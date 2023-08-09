39b199e6a9fe1497ac689b342be4513959207ce0
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-09 11:58:58 +0100

    Update require_path regex check

--- 0x01-Basic_authentication/main.py
+++ 0x01-Basic_authentication/main.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python3
-""" Main 100
+#!/usr/bin/python3
+""" Check response
 """
-import base64
-from api.v1.auth.basic_auth import BasicAuth
-from models.user import User
 
-""" Create a user test """
-user_email = "bob100@hbtn.io"
-user_clear_pwd = "H0lberton:School:98!"
+if __name__ == "__main__":
+    from api.v1.auth.auth import Auth
 
-user = User()
-user.email = user_email
-user.password = user_clear_pwd
-print("New user: {}".format(user.id))
-user.save()
-
-basic_clear = "{}:{}".format(user_email, user_clear_pwd)
-print("Basic Base64: {}".format(base64.b64encode(basic_clear.encode('utf-8')).decode("utf-8")))
+    a = Auth()
+    path = "/api/v1/uas"
+    paths_excluded = ["/api/v1/us*"]
+    res = a.require_auth(path, paths_excluded)
+    print(res)
+    if not res:
+        print("require_auth must return False: {} - {}".format(path, paths_excluded))
+        exit(1)
+    print("OK", end="")
+    
