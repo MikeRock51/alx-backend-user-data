901917d49693ece72328769d86b16fe8169de4fe
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-15 21:52:22 +0100

    Add Task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,17 +1,33 @@
 #!/usr/bin/env python3
 """Flask Application"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from auth import Auth
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False)
 def home():
     """Home route"""
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def createUser():
+    """Creates a new user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    return jsonify({"email": email, "message": "user created"}), 201
 
 
 if __name__ == "__main__":
