f4bb22694d37d7326d07f72ed9f8f67124f6f516
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-16 18:38:49 +0100

    Update reset_token=None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -97,6 +97,6 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
             self._db.update_user(user.id, hashed_password=hashed_password,
-                                 reset_token=reset_token)
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
