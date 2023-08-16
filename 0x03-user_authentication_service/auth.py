5ebd08808ebc8f91a4ca749adfcd374ecbdfe817
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-16 10:23:23 +0100

    Add Task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,12 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except ValueError:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Generates a reset_token for a user based on email"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
