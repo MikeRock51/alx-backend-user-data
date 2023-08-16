52f34a76757146f9f7b30f87583817b298531d49
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-16 08:16:40 +0100

    Add Task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,3 +59,14 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Retireves the user with the session_id"""
+        if not session_id:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
