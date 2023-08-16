2e5d09b3ac9743b4655cc39ce604f7dc1aa75bac
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-16 05:44:55 +0100

    Add task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask Application"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 from auth import Auth
 
@@ -28,6 +28,24 @@ def createUser():
         return jsonify({"message": "email already registered"}), 400
 
     return jsonify({"email": email, "message": "user created"}), 201
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def getSession():
+    """Creates a session for the user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email or not password:
+        abort(400)
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    res = jsonify({"email": email, "message": "logged in"})
+    res.set_cookie('session_id', session_id)
+
+    return res
 
 
 if __name__ == "__main__":
