ab0cdbd83b17d225c423e4694bf99b328c328a0b
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-16 10:28:07 +0100

    Update Task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,5 +87,5 @@ class Auth:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
-        except NoResultFound:
+        except Exception:
             raise ValueError
