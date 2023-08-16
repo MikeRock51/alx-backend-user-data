db5e569466111aa69d695246bba546ea0c6fae30
MikeRock51 <mikerockmusic51@gmail.com>
2023-08-16 10:03:01 +0100

    Add Task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,5 +59,16 @@ def logout() -> str:
     return redirect('/')
 
 
+@app.route('/profile', strict_slashes=False)
+def userProfile():
+    """Returns the user profile based of session_id"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
